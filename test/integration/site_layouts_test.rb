105d017a2286f6ffd72d3fafc0df58b5cb8f5c11
Nishiyama-i3 <nishiyama@i3-systems.com>
2020-12-14 11:36:57 +0900

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layouts_test.rb
+++ test/integration/site_layouts_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutsTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
   test 'layout links' do
     get root_path
     assert_template 'static_pages/home'
@@ -8,6 +12,36 @@ class SiteLayoutsTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
+    get contact_path
+    assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select 'title', full_title('Sign up')
+  end
+  test 'layout links with logged in' do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path, count:2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', logout_path
+    get contact_path
+    assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select 'title', full_title('Sign up')
+  end
+
+  test 'layout links without logged in' do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path, count:2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', login_path
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
